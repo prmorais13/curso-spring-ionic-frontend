b019be08ae13a0059757dab4909ab16128ad7ed3
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-01-11 16:56:15 -0300

    Listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { CategoriaService } from '../../services/domain/categoria.service';
+
 @IonicPage()
 @Component({
   selector: 'page-categorias',
@@ -8,11 +10,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public categoriaService: CategoriaService
+  ) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    this.categoriaService.findAll()
+      .subscribe(response => {
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      })
   }
 
 }
