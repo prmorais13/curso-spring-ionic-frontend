178229254120d57c42cd3cc63165104d277ea2df
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-02-01 21:59:14 -0200

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -15,6 +15,10 @@ export class ClienteService {
       public storageService: StorageService
    ) {}
 
+   findById(id: string) {
+         return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+   }
+
    findByEmail(email: string) {
          return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?email=${email}`);
    }
