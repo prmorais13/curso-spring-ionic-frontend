5da51678f5385bccc97231141ffa9ca3d479e646
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-02-02 23:21:32 -0200

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -23,6 +23,10 @@ export class ProdutosPage {
   ) { }
 
   ionViewDidLoad() {
+   this.loadData()
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -57,5 +61,13 @@ export class ProdutosPage {
     loader.present();
     return loader;
   }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 100);
+  }
+
 
 }
